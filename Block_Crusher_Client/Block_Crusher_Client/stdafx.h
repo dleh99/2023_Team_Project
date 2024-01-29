042fd37a8198272682ae3b9498594d51b5a549d8
oychan <oyc0507@gmail.com>
2024-01-29 20:43:35 +0900

    임시

--- Block_Crusher_Client/Block_Crusher_Client/stdafx.h
+++ Block_Crusher_Client/Block_Crusher_Client/stdafx.h
@@ -84,6 +84,13 @@ inline void Swap(float* pfS, float* pfT) { float fTemp = *pfS; *pfS = *pfT; *pfT
 #define ANIMATION_TYPE_LOOP				1
 #define ANIMATION_TYPE_PINGPONG			2
 
+#define MAX_LIGHTS 8
+#define MAX_MATERIALS 8
+
+#define POINT_LIGHT 1
+#define SPOT_LIGHT 2
+#define DIRECTIONAL_LIGHT 3
+
 // 정의하면 처음 실행할 때 전체화면 모드로 실행
 // #define _WITH_SWAPCHAIN_FULLSCREEN_STATE
 extern UINT	gnCbvSrvDescriptorIncrementSize;
